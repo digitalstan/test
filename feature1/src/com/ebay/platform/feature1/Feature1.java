566b57ff5021a40d9de8e0812736038efd00ef3a
Stan <shsu@ebay.com>
2014-05-28 11:19:47 -0700

    main class
    initial

---
+++ feature1/src/com/ebay/platform/feature1/Feature1.java
@@ -0,0 +1,10 @@
+package com.ebay.platform.feature1;
+
+public class Feature1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
